245b84e79b5a1d75162957c1905c568995275cb9
Amirjon <166128814+AmirTursunov@users.noreply.github.com>
2025-05-22 22:54:45 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,30 @@ let quote = document.getElementById("quote");
 let author = document.getElementById("author");
 let btn = document.getElementById("btn");
 
-const url = "https://api.quotable.io/random";
-
+const url = "https://api.api-ninjas.com/v1/quotes";
+const apiKey = "fCsau9c+YMHlAGBznJOp+Q==zm0bMCj5P4GdrHPp";
 let getQuote = () => {
-  fetch(url)
-    .then((data) => data.json())
-    .then((item) => {
-      quote.innerText = item.content;
-      author.innerText = item.author;
+  btn.innerText = "Loading...";
+  fetch(url, {
+    headers: {
+      "X-Api-Key": apiKey,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.length > 0) {
+        quote.innerText = data[0].quote;
+        author.innerText = data[0].author;
+        btn.innerText = "Get Quote";
+      } else {
+        quote.innerText = "Hech qanday iqtibos topilmadi.";
+        author.innerText = "";
+      }
+    })
+    .catch((error) => {
+      quote.innerText = "Xatolik yuz berdi!";
+      author.innerText = "";
+      console.error("Xatolik:", error);
     });
 };
 
